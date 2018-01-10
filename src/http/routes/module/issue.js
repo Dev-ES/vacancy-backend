e45cc24d0900a7f2b0c6e726aa9769ec2c6d8086
ysouzas <yago.souza.santos@gmail.com>
2018-01-10 10:38:32 -0200

    add .env

--- src/http/routes/module/issue.js
+++ src/http/routes/module/issue.js
@@ -7,14 +7,27 @@ module.exports = function issue(server) {
       url: 'https://api.github.com/repos/ysouzas/hangout-vagas/issues',
       headers: {
         'User-Agent': 'request',
+        Authorization: `token ${process.env.TokenGITHUB}`,
       },
     });
-
-
     client.get('', (err, request, response, obj) => {
       res.json(obj);
     });
+    next();
+  });
 
+
+  server.post('/issues', (req, res, next) => {
+    const client = clients.createJsonClient({
+      url: 'https://api.github.com/repos/ysouzas/hangout-vagas/issues',
+      headers: {
+        'User-Agent': 'request',
+        Authorization: `token ${process.env.TokenGITHUB}`,
+      },
+    });
+    client.post('', req.body, (err, request, response, obj) => {
+      res.json(obj);
+    });
     next();
   });
 };
