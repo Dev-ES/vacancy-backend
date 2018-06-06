e34a47e53448840298ac53ad3cfcca98af57ab5a
Gary M. Silva <garymsilva@outlook.com>
2018-06-06 19:09:49 -0300

    Remove 'localhost'

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import http from 'http';
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, 'localhost');
+}).listen(5000);
 
-console.log('Server running at http://localhost:1337/');
+console.log('Server running...');
