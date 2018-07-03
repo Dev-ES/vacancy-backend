f9f9bdf4d450dd7cab2d428ad0aae9b4efcf7430
JoséGuilherme <guislima47@gmail.com>
2018-07-03 20:04:39 -0300

    Create Requirement Module

--- src/modules/vacancy/Vacancy.ts
+++ src/modules/vacancy/Vacancy.ts
@@ -2,6 +2,7 @@ import Benefit from "./../benefit/Benefit";
 
 class Vacancy {
   id: number;
+  idContratant: number;
   title: string;
   description: String;
   location: String;
@@ -11,10 +12,12 @@ class Vacancy {
 
   constructor(data: any) {
     this.id = data.id;
+    this.idContratant = data.idContratant;
     this.title = data.title;
     this.description = data.description;
     this.location = data.location;
     this.howToApply = data.howToApply;
+
     // create benefits list
     this.benefits = data.benefits != undefined ? data.benefits.map(
       (benef) => { return new Benefit(benef, data.id); }
