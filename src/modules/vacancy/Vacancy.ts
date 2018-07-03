8881d403b854824ecd221f6a227eda08055bcb5e
JoséGuilherme <guislima47@gmail.com>
2018-07-03 00:28:27 -0300

    Corrections in Vacancy Class Constructor

--- src/modules/vacancy/Vacancy.ts
+++ src/modules/vacancy/Vacancy.ts
@@ -3,18 +3,22 @@ import Benefit from "./../benefit/Benefit";
 class Vacancy {
   id: number;
   title: string;
+  description: String;
+  location: String;
+  howToApply: String;
+  alocado: boolean;
   benefits: Benefit[];
 
   constructor(data: any) {
     this.id = data.id;
     this.title = data.title;
+    this.description = data.description;
+    this.location = data.location;
+    this.howToApply = data.howToApply;
+    // create benefits list
     this.benefits = data.benefits != undefined ? data.benefits.map(
-      (benef) => { return new Benefit(benef); }
+      (benef) => { return new Benefit(benef, data.id); }
     ) : undefined;
-    console.log(this.benefits);
   }
-
 }
-
-
 export default Vacancy;
